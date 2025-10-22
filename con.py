145d6617e798184b6a6aca73ce0f68300b375b9c
Ranjitha D <dranjithad071@gmail.com>
2025-10-21 22:57:40 -0700

    Add files via upload

---
+++ con.py
@@ -0,0 +1,23 @@
+x = 23
+if x%2==0:
+    print("x ix even")
+else: 
+    print("x is odd")
+
+
+# by using signals
+signal = "red"
+if signal == "red":
+    print("stop")
+else:
+    print("go")
+
+#traffic
+signal = input("what ' s the colour of the signal >>")
+
+if signal == "red":
+    print("stop")
+elif signal == "yellow":
+    print("get ready")
+elif signal == "green":
+    print("go")
