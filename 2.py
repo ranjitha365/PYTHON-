cfe18b8c5102d2d9a2604fe8dcff2aff14c7a3fc
Ranjitha D <dranjithad071@gmail.com>
2025-10-25 06:23:25 -0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,20 @@
+is_failed = True
+
+i = 1
+while is_failed :
+    print(f"tr{i}")
+
+    i = i+1
+    
+
+is_failed = True
+i =1
+
+while is_failed:
+    print(f"try {i}")
+    i = i + 1
+    if i >=100:
+        break
+        print("I gave up!")
+    
+    
