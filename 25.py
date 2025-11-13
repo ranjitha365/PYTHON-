1f54dc523cac107a852398ab1c86cb032b7e9f17
Ranjitha D <dranjithad071@gmail.com>
2025-11-13 08:28:37 -0800

    Add files via upload

--- 25.py
+++ 25.py
@@ -1,9 +1,8 @@
-# by taking input in while loop
-pin = "1234"
-
-input_pin = input("pin>> ")
-
-if input_pin == pin:
-    print("CORRECT")
-else:
-    print("INCORRECT")
+# for removing duplicated dictionary
+data = {'a':'1', 'b':'2', 'c':'1'}
+unique = {}
+for key, value in data.items():
+    if list(data.values()).count(value) == 1:
+        unique[key] = value
+        print(unique)
+    
