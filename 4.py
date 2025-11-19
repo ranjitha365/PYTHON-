cbe198ad69f4801466b4843b7e4c119bb83f7666
Ranjitha D <dranjithad071@gmail.com>
2025-11-19 08:49:16 -0800

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,9 +1,32 @@
-is_failed = True
-i = 0
+file = open("notes.txt", "r")
+contents  = file.read()       # to read a file
+print(contents)               # to get an output
+file.close()                  # closing for files
 
-while is_failed and i <= 10:
-    if i % 2 == 0:
-        print(f"attempt: {i}")
-        i += 1
+file = open("notes.txt", "r")
+contents = file.readlines()  
+ # to read multiple of lines                      # of the same file 
+print(contents)
+file.close() 
+
+
+file = open("notes.txt", "w") # write a code
+file.write("ranjirtha") # hey guys this is ranjitha
+file.close() 
+
+
+
+
+file = open("notes.txt", "a") # append means if we are forgetting to add something agan we can put
+file.write("\n,yashi")
+file.close()
+
+
+# using a "with key word"
+with open("notes.txt", "r") as f :
+    print(f.read())
+
+
+
 
 
