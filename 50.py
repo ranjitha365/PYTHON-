df4aa3b6591890a1921bff1573feb2bfe8cce5e9
Ranjitha D <dranjithad071@gmail.com>
2025-11-24 18:53:26 -0800

    Add files via upload

--- 50.py
+++ 50.py
@@ -1,17 +1,9 @@
-# simple while loops
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
+file = open("notes.text", "r")
+contents = file.read()
+print(contents)
+file.close()
 
 
 
-
-i = 1
-while i <= 10:
-    print("chandan"*i)
-    i = i + 1
-
-
-
+    
 
