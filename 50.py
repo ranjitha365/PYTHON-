d1c3ba8603d0b3e7f1163aafac91cb386a70fa66
Ranjitha D <dranjithad071@gmail.com>
2026-01-03 08:21:51 -0800

    Add files via upload

--- 50.py
+++ 50.py
@@ -1,7 +1,30 @@
-file = open("notes.text", "r")
-contents = file.read()
-print(contents)
-file.close()
+# Magic method
+# Initialisation and object creation method
+
+class A:
+
+    def __init__(self,X):
+        self.X = X
+obj = A(10)
+print(obj.X)
+
+
+
+
+# string representation method
+
+class A:
+    def __init__(self,x):
+        self.x = x
+    def __str__(self):
+        return f"value is {self.x}"
+    
+obj = A(15)
+print(obj)
+
+
+
+
 
 
 
